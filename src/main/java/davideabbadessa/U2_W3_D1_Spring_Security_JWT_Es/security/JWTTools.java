b9a56c8d41e71eedd1aa2052d7967061e6f0811d
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-01 15:21:56 +0200

    config JWTTTOOLS

--- src/main/java/davideabbadessa/U2_W3_D1_Spring_Security_JWT_Es/security/JWTTools.java
+++ src/main/java/davideabbadessa/U2_W3_D1_Spring_Security_JWT_Es/security/JWTTools.java
@@ -1,8 +1,13 @@
 package davideabbadessa.U2_W3_D1_Spring_Security_JWT_Es.security;
 
 
+import davideabbadessa.U2_W3_D1_Spring_Security_JWT_Es.entities.Dipendente;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JWTTools {
@@ -10,5 +15,14 @@ public class JWTTools {
     @Value("${jwt.secret}")
     private String secret;
 
+
+    public String creaToken(Dipendente dipendente) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 14))
+                .subject(String.valueOf(dipendente.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
 
 }
