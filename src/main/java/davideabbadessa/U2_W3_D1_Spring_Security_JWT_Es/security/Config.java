0f209d8c92995ae069166dc25b6d11235649e805
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-01 15:09:47 +0200

    add Config

---
+++ src/main/java/davideabbadessa/U2_W3_D1_Spring_Security_JWT_Es/security/Config.java
@@ -0,0 +1,25 @@
+package davideabbadessa.U2_W3_D1_Spring_Security_JWT_Es.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Config {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        // Disabilitare alcune funzionalità di default
+        httpSecurity.formLogin(http -> http.disable());
+        httpSecurity.csrf(http -> http.disable());
+        httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll()); // <- questo evita di avere 401 per ogni richiesta
+
+        return httpSecurity.build();
+    }
+
+}
---
+++ src/main/java/davideabbadessa/U2_W3_D1_Spring_Security_JWT_Es/security/JWTTools.java
@@ -0,0 +1,9 @@
+package davideabbadessa.U2_W3_D1_Spring_Security_JWT_Es.security;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTTools {
+    
+}
